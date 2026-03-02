c0e8ff96a4f9110632aaa56148192534858819fa
Abhayy <eyeronic2772@gmail.com>
2026-03-02 10:35:54 +0530

    Create addition.sol

---
+++ LabWork/addition.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Addition {
+    function add(uint256 _a, uint256 _b) public pure returns (uint256) {
+        return _a + _b;
+    }
+}
