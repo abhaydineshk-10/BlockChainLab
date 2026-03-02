3707dc987dbcc25e102360284860f3b070a50be4
Abhayy <eyeronic2772@gmail.com>
2026-03-02 10:39:37 +0530

    Create squarenumber.sol

---
+++ LabWork/squarenumber.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SquareNumber {
+    function square(uint256 _number) public pure returns (uint256) {
+        return _number * _number;
+    }
+}
